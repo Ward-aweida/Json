39f30488db5909d1fa755a0cf39e398fd28a31ea
ward awiedah <78215331+Ward-aweida@users.noreply.github.com>
2021-07-08 14:43:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/json/ExampleItem.java
@@ -0,0 +1,21 @@
+package com.example.json;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExampleItem  {
+    private String mLine1;
+    private String mLine2;
+    public ExampleItem(String line1, String line2) {
+        mLine1 = line1;
+        mLine2 = line2;
+    }
+    public String getLine1() {
+        return mLine1;
+    }
+    public String getLine2() {
+        return mLine2;
+    }
+
+}
